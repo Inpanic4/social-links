830a0df3bd8238a4f3fd2a2b8984f24bbab83b84
Stavros Argyropoulos <argy@Stavross-Mini.home>
2024-02-03 12:27:37 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import ListItem from './ListItem';
 function App() {
   return (
     <div className="flex justify-center items-center h-screen bg-neutral-offBlack text-neutral-white">
-      <div className="bg-neutral-darkGrey text-center px-12 pb-7 pt-12 rounded-2xl ">
-        <img src="/images/avatar-jessica.jpeg" alt="Jessica" className="mx-auto rounded-full size-24 mb-8" />
+      <div className="bg-neutral-darkGrey text-center px-4 sm:px-12 pb-7 pt-12 rounded-2xl">
+        <img src="/social-links/images/avatar-jessica.jpeg" alt="Jessica" className="mx-auto rounded-full size-24 mb-8" />
         <div className="font-bold mb-2 text-2xl">Jessica Randall</div>
          <div className='text-primary-green mb-8 text-sm'>London, United Kingdom</div>
-        <p className='mb-8 text-sm'>"Front-end developer and avid reader."</p>
+        <p className='font-thin mb-8 text-sm px-4 sm:px-8'>"Front-end developer and avid reader."</p>
+        
         <ul className=' '>
         <ListItem name='GitHub'/>
           <ListItem name='FrontendMentor'/>
@@ -16,6 +17,7 @@ function App() {
           <ListItem name='Twitter'/>
           <ListItem name='Instagram'/>
         </ul>
+        
       </div>
     </div>
   );
--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -5,7 +5,7 @@ type ListItemProps = {
 };
 
 const ListItem: React.FC<ListItemProps> = ({ name }) => {
-  return <li className='py-3 my-4 bg-neutral-grey rounded-lg'>{name}</li>;
+  return <li className='py-3  my-4 bg-neutral-grey rounded-lg'>{name}</li>;
 };
 
 export default ListItem;
